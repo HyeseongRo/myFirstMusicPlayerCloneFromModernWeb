6d66e69b7cb7333b4a1c29c1658eab1fea72f6fd
HyeseongRo <74771093+HyeseongRo@users.noreply.github.com>
2022-10-17 22:03:24 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,128 @@
+let songs = [
+    {
+        name: 'OOH-AHH하게',
+        path: 'assets/musics/TWICE - 01 - OOH-AHH하게-201510.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_THE_STORY_BEGINS.jpeg'
+    },
+    {
+        name: '미쳤나봐',
+        path: 'assets/musics/TWICE - 03 - 미쳤나봐-201510.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_THE_STORY_BEGINS.jpeg'
+    },
+    {
+        name: 'Candy Boy',
+        path: 'assets/musics/TWICE - 05 - Candy Boy-201510.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_THE_STORY_BEGINS.jpeg'
+    },
+    {
+        name: 'Like a Fool',
+        path: 'assets/musics/TWICE - 06 - Like a Fool-201510.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_THE_STORY_BEGINS.jpeg'
+    },
+    {
+        name: 'CHEER UP',
+        path: 'assets/musics/TWICE - 01 - CHEER UP-201604.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_PAGE_TWO.jpeg'
+    },
+    {
+        name: 'TT',
+        path: 'assets/musics/TWICE(트와이스) - 01 - TT-201610.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_TWICEcoaster_LANE_1.jpeg'
+    },
+    {
+        name: '1 TO 10',
+        path: 'assets/musics/TWICE(트와이스) - 02 - 1 TO 10-201610.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_TWICEcoaster_LANE_1.jpeg'
+    },
+    {
+        name: 'JELLY JELLY',
+        path: 'assets/musics/TWICE(트와이스) - 04 - JELLY JELLY-201610.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_TWICEcoaster_LANE_1.jpeg'
+    },
+    {
+        name: 'PONYTAIL',
+        path: 'assets/musics/TWICE(트와이스) - 03 - PONYTAIL-201610.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_TWICEcoaster_LANE_1.jpeg'
+    },
+    {
+        name: 'PIT-A-PAT',
+        path: 'assets/musics/TWICE(트와이스) - 05 - PIT-A-PAT-201610.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_TWICEcoaster_LANE_1.jpeg'
+    },
+    {
+        name: 'ONE IN A MILLION',
+        path: 'assets/musics/TWICE(트와이스) - 07 - ONE IN A MILLION-201610.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_TWICEcoaster_LANE_1.jpeg'
+    },
+    {
+        name: 'KNOCK KNOCK',
+        path: 'assets/musics/TWICE (트와이스)-01-KNOCK KNOCK-TWICEcoaster _ LANE 2-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_TWICEcoaster_LANE_2.jpeg'
+    },
+    {
+        name: '녹아요',
+        path: 'assets/musics/TWICE (트와이스)-02-녹아요-TWICEcoaster _ LANE 2-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_TWICEcoaster_LANE_2.jpeg'
+    },
+    {
+        name: 'SOMEONE LIKE ME',
+        path: 'assets/musics/TWICE (트와이스)-06-SOMEONE LIKE ME-SIGNAL-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_SIGNAL.jpeg'
+    },
+    {
+        name: '거북이',
+        path: 'assets/musics/TWICE (트와이스)-02-거북이-twicetagram-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_twicetagram.jpeg'
+    },
+    {
+        name: 'MISSING U',
+        path: 'assets/musics/TWICE (트와이스)-03-MISSING U-twicetagram-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_twicetagram.jpeg'
+    },
+    {
+        name: 'WOW',
+        path: 'assets/musics/TWICE (트와이스)-04-WOW-twicetagram-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_twicetagram.jpeg'
+    },
+    {
+        name: 'Heart Shaker',
+        path: 'assets/musics/TWICE (트와이스)-01-Heart Shaker-Merry & Happy-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_Merry_&_Happy.jpeg'
+    },
+    {
+        name: 'What is Love?',
+        path: 'assets/musics/TWICE (트와이스)-01-What is Love_-What is Love_-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_What_is_Love.jpeg'
+    },
+    {
+        name: 'STUCK',
+        path: 'assets/musics/TWICE (트와이스)-06-STUCK-What is Love_-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_What_is_Love.jpeg'
+    },
+    {
+        name: 'Dance The Night Away',
+        path: 'assets/musics/TWICE (트와이스)-01-Dance The Night Away-Summer Nights-320.mp3',
+        artist: 'TWICE',
+        cover: 'assets/images/TWICE_Summer_Nights.jpeg'
+    },
+]
